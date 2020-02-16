c05af76f4d64e7c1bc26337bed1441462cbdd3c3
hikaru7719 <j148016s@st.u-gakugei.ac.jp>
2020-02-16 11:35:19 +0900

    mod

---
+++ phrases/src/main.rs
@@ -0,0 +1,11 @@
+extern crate phrases;
+
+use phrases::english::{farewells, greetings};
+use phrases::japanese;
+fn main() {
+    println!("Hello in English {}", greetings::hello());
+    println!("Goodbye in English {}", farewells::goodbye());
+
+    println!("Hello in Japanese {}", japanese::hello());
+    println!("Goodbye in Japanese {}", japanese::goodbye());
+}
